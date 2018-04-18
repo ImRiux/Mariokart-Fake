85b6956606b4915e7f4eb43855d4a98af217fe52
abgestaubt <38501785+abgestaubt@users.noreply.github.com>
2018-04-18 17:27:16 +0200

    Add files via upload
    AMENAKOI

---
+++ race/src/Player.cpp
@@ -0,0 +1,33 @@
+#include "Player.h"
+
+Player::Player() {
+
+}
+
+Player::~Player() {
+
+}
+
+void Player::setX(int x) {
+    this->x = x;
+}
+
+void Player::setY(int y) {
+    this->y = y;
+}
+
+void Player::changeX(int x) {
+    this->x += x;
+}
+
+void Player::changeY(int y) {
+    this->y -= y;
+}
+
+int Player::getX() const {
+    return this->x;
+}
+
+int Player::getY() const {
+    return this->y;
+}
